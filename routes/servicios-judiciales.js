8f46aa290f78ff3ce24679e5bb7c30f13c551d48
Paul Villalta <paul.villalta@ucuenca.edu.ec>
2023-07-17 14:40:08 -0500

    Update servicios-judiciales.js

--- routes/servicios-judiciales.js
+++ routes/servicios-judiciales.js
@@ -12,4 +12,17 @@ router.get('/categorias',(req, res)=>{
     });
 });
 
+router.get('/ubicaciones/:id_categoria',(req, res)=>{
+    const { id_categoria } = req.params;
+    let sql = `SELECT * FROM ubicaciones_servicios_judiciales WHERE id_categoria=${id_categoria}`;
+    
+    conexion
+    conexion.query(sql,(err, result)=>{
+        if(err) throw err;
+        else{
+            res.json(result.rows);
+        }
+    });
+});
+
 module.exports=router;
