45336bc1c2402aa5af541e485fce5185d8566515
Paul Villalta <paul.villalta@ucuenca.edu.ec>
2023-07-20 12:59:03 -0500

    correciones autenticacion

--- routes/autenticacion.js
+++ routes/autenticacion.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const conexion = require('../config/conexion');
 
-router.post('/registro-usuario', (req,res) => {
+router.post('/registro-usuario', async (req,res) => {
     const request = req.body
 
-    const usuarioExiste = verificarUsuarioExiste(request.email)
+    const usuarioExiste = await verificarUsuarioExiste(request.email)
     if(usuarioExiste) {
         res.status(400)
         .json({
@@ -13,12 +13,16 @@ router.post('/registro-usuario', (req,res) => {
         return
     }
 
-    const maxId = obtenerMaxIdUsuario()
+    const maxId = await obtenerMaxIdUsuario()
+    console.log('Max Id', maxId)
     const nuevoId = maxId + 1
 
     const sql = `INSERT INTO abogado(id_abogado, nombres, apellidos, email, password) 
         VALUES ($1,$2,$3,$4,$5)`;
-    conexion.query(sql, [nuevoId, request.nombres, request.apellidos, request.email, request.password], (err, result) => {
+
+    console.log('Body', request)
+    console.log('NuevoId', nuevoId)
+    conexion.query(sql, [nuevoId, request.nombres, request.apellidos, request.email, request.clave], (err, result) => {
         if(err) throw err
         else {
             res.status(200)
@@ -29,26 +33,26 @@ router.post('/registro-usuario', (req,res) => {
     });
 });
 
-function verificarUsuarioExiste(correo) {
+async function verificarUsuarioExiste(correo) {
     const sql = "SELECT count(*) nro_usuarios FROM abogado WHERE email=$1";
-    conexion.query(sql, [correo], (err, result) => {
-        if(err) throw err
-        else {
-            const nroUsuarios = result.rows[0]
-            return nroUsuarios > 0
-        }
-    })
+    try {
+        const result = await conexion.query(sql, [correo])
+        const nroUsuarios = result.rows[0].nro_usuarios
+        return nroUsuarios > 0
+    } catch(err) {
+        throw err
+    }
 }
 
-function obtenerMaxIdUsuario() {
+async function obtenerMaxIdUsuario() {
     const sql = "SELECT coalesce(max(id_abogado), 0) max_id FROM abogado";
-    conexion.query(sql, (err, result) => {
-        if(err) throw err
-        else {
-            const maxId = result.rows[0]
-            return maxId
-        }
-    })
+    try {
+        const result = await conexion.query(sql)
+        const maxId = result.rows[0].max_id
+        return maxId
+    } catch(err) {
+        throw err
+    }
 }
 
 module.exports=router;
